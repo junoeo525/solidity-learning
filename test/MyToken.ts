dcfcfdb2d9bd3fef304beb478f0420db006b0dd0
유준상 <yoojunsang@yujunsang-ui-MacBookAir.local>
2026-01-01 13:07:58 +0900

    transfer emit test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -1,4 +1,4 @@
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 import { expect } from "chai";
 import { MyToken } from "../typechain-types";
 import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers";
@@ -44,12 +44,23 @@ describe("My Token", () => {
   }) 
   describe("Transfer", () => {
   it("should have 0.5MT", async () => {
+    const signer0 = signers[0];
     const signer1 = signers[1];
-    await myTokenC.transfer(hre.ethers.parseUnits("0.5", decimals), signer1.address);
+    await expect(
+        myTokenC.transfer(
+            hre.ethers.parseUnits("0.5", decimals), 
+            signer1.address)
+        )
+        .to.emit(myTokenC, "Transfer")
+        .withArgs(
+            signer0.address,
+            signer1.address, 
+            hre.ethers.parseUnits("0.5",decimals)
+        );
     expect(await myTokenC.balanceOf(signer1)).equal(
         hre.ethers.parseUnits("0.5", decimals)
     );
-  });
+});
   it("it shoud be reverted with insufficient balance error", async () => {
     const signer1 = signers[1];
     await expect(
