a0df716373c05466111bc6428d3bd1ee852c7dcb
유준상 <yoojunsang@yujunsang-ui-MacBookAir.local>
2026-01-01 11:20:35 +0900

    mint function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -9,16 +9,21 @@ contract MyToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        uint8 _decimal
-    ) {
+    constructor(string memory _name,string memory _symbol,uint8 _decimal) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
+        // transaction
+        // from, to, data, value, gas, ...
+        _mint(1*10**uint256(decimals), msg.sender); // 1 MT
     }
-
+    
+    function _mint(uint256 amount, address owner) internal {
+    // totalSupply = totalSupply + amount;
+    // balanceOf[owner] = balanceOf[owner] + amount;
+        totalSupply += amount;
+        balanceOf[owner] += amount;
+    }
     // function totalSupply() external view returns (uint256) {
     //     return totalSupply;
     // }
