642e68f7ee4f37e7f3249ba0f761791e34ee1d50
유준상 <yoojunsang@yujunsang-ui-MacBookAir.local>
2026-01-01 13:08:39 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,31 +2,58 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
-    uint8 public decimals; // uint8 --> 8 bit unsigned int, uint16, ... , uint256
+    uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => unit256)) allowance;
 
-    constructor(string memory _name,string memory _symbol,uint8 _decimal, uint256 _amount) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimal,
+        uint256 _amount
+    ) {
         name = _name;
         symbol = _symbol;
         decimals = _decimal;
-        _mint(_amount*10**uint256(decimals), msg.sender); // 1 MT
+
+        _mint(_amount * 10 ** uint256(decimals), msg.sender);
     }
-    
+
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender] [spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
     function _mint(uint256 amount, address owner) internal {
-    // totalSupply = totalSupply + amount;
-    // balanceOf[owner] = balanceOf[owner] + amount;
         totalSupply += amount;
         balanceOf[owner] += amount;
+        
+        emit Transfer(address(0), owner, amount);
     }
+
     function transfer(uint256 amount, address to) external {
-    require(balanceOf[msg.sender] >= amount, "insufficient balance");
-    
-    balanceOf[msg.sender] -= amount;
-    balanceOf[to] += amount;
+        require(balanceOf[msg.sender] >= amount, "insufficient balance");
+
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
     }
 }
  
