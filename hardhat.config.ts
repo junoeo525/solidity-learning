4acf58d18fefe7c1049923ad1bd9f9c5f54ba9f6
유준상 <yoojunsang@yujunsang-ui-MacBookAir.local>
2026-01-09 11:12:25 +0900

    Complete Vyper-based contracts and archive Solidity implementation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,6 +7,29 @@ const config: HardhatUserConfig = {
   vyper: {
     version: "0.3.0",
   },
-};
+  networks: {
+    kairos: {
+      url: "https://public-en-kairos.node.kaia.io",
+      accounts: [
+        "0x99682c999e36059bdac25df003719aed9d62cb39efbb377508ab47369db274b5"
+      ],
+    },
+  },
+    etherscan: {
+        apiKey: {
+          kairos: "unnecessary",
+        },
+        customChains: [
+          {
+            network: "kairos",
+            chainId: 1001,
+            urls: {
+              apiURL: "https://kairos-api.kaiascan.io/hardhat-verify",
+              browserURL: "https://kairos.kaiascan.io",
+            }
+          },
+        ]
+      }
+  };
 
 export default config;
